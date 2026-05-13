9ab85475c387641e9c51ce35b1548ccd1eac50c8
MrPelmenis <46497841+MrPelmenis@users.noreply.github.com>
2026-05-13 12:41:10 +0200

    Google JWT strada?
    ludzu strada laptop

--- FrontEnd/coolspot/src/Header.js
+++ FrontEnd/coolspot/src/Header.js
@@ -8,7 +8,7 @@ function Header() {
   };
 
   return (
-    <GoogleOAuthProvider clientId="YOUR_CLIENT_ID">
+    <GoogleOAuthProvider clientId="304862924981-o5ghsqptv2e8jjbkvli6cm0rov256ahv.apps.googleusercontent.com">
     <header className="bg-gray-200 py-4 w-full flex justify-between items-center px-4 rounded-b-lg shadow-md sticky top-0 z-10">
       <h1 className="text-4xl font-bold text-gray-800 flex-grow text-center md:text-left">CoolSpot</h1>
       <GoogleLogin
