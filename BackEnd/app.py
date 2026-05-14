4ac18544928174e60707785a50334d4c54b8868b
Maximilian Shruster <shrustermaksimilian@gmail.com>
2026-05-14 04:05:52 +0200

    added build, now you can see react page from flask port

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -1,23 +1,32 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_from_directory
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../Frontend/coolspot/build', template_folder='../Frontend/coolspot/build')
 CORS(app)  # Enables Cross-Origin Resource Sharing to allow requests from different ports (e.g., React on port 3000)
 
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    return jsonify({
-        'message': 'Hello from Flask',
-        'data': [1, 2, 3, 4]
-    })
 
-@app.route('/api/post-data', methods=['POST'])
-def post_data():
-    received_data = request.json
-    return jsonify({
-        'message': 'Data received',
-        'data': received_data
-    })
+@app.route('/')
+def serve_react_app():
+    return send_from_directory(app.template_folder, 'index.html')
+
+@app.route('/static/<path:path>')
+def serve_static_files(path):
+    return send_from_directory(app.static_folder + '/static', path)
+
+# @app.route('/api/data', methods=['GET'])
+# def get_data():
+#     return jsonify({
+#         'message': 'Hello from Flask',
+#         'data': [1, 2, 3, 4]
+#     })
+
+# @app.route('/api/post-data', methods=['POST'])
+# def post_data():
+#     received_data = request.json
+#     return jsonify({
+#         'message': 'Data received',
+#         'data': received_data
+#     })
 
 if __name__ == '__main__':
     app.run(debug=True)
